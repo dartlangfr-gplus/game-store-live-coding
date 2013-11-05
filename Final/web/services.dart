ace483674554cdbdf9e180865ad20bf65823340f
ggirou <porude-git@yahoo.fr>
2013-11-05 22:45:54 +0100

    Step 8 persist using a rest API

--- Final/web/services.dart
+++ Final/web/services.dart
@@ -1,30 +1,15 @@
 library game_store.service;
 
-import 'dart:math';
 import 'dart:async';
+import 'dart:html';
+import 'dart:convert';
 import 'package:game_store/models.dart';
 
 class GameStoreService {
-  final Map<int, Game> games = new Map.fromIterable([
-    new Game()..id = 1..name = "Darts"..genre = "Pub game"..description = 'Darts is ...'..image = "darts.jpg"..rating = 5,                    
-    new Game()..id = 2..name = "Chess"..genre = "Board game"..description = 'Chess is ...'..image = "chess.jpg"..rating = 4,                    
-    new Game()..id = 3..name = "Dices"..genre = "Random game"..description = 'Dice are ...'..image = "dice.jpg"..rating = 3,                    
-    new Game()..id = 4..name = "Go"..genre = "Board game"..description = 'Go is ...'..image = "go.jpg"..rating = 2,
-    new Game()..id = 5..name = "Poker"..genre = "Card game"..description = 'Poker is ..'..image="poker.jpg"..rating = 4,
-    new Game()..id = 6..name = "Pool"..genre = "Pub game"..description = 'Pool is ..'..image="pool.jpg"..rating = 3,
-    new Game()..id = 7..name = "Bingo"..genre = "Boring game"..description = 'Bingo is ..'..image="bingo.jpg"..rating = 1
-  ], key: (g) => g.id);
-  
-  Future<List<Game>> getAll() => new Future.value(games.values.toList());
-  Future<Game> getById(int id) => new Future.value(games[id]);
-  Future<Game> save(Game game) => new Future(() {
-    if(game.id == null) {
-      game.id = games.values.map((g) => g.id).fold(0, max) + 1;
-    }
-    games[game.id] = game;
-    return game;
-  });
-  Future<Game> delete(int gameId) => new Future.value(games.remove(gameId));
+  Future<List<Game>> getAll() => HttpRequest.getString("api/games.json").then(JSON.decode).then((List list) => list.map(Game.fromMap).toList());
+  Future<Game> getById(int id) => HttpRequest.getString("api/games/$id.json").then(JSON.decode).then(Game.fromMap);
+  Future<Game> save(Game game) => HttpRequest.request("api/games.json", method: "PUT", sendData: JSON.encode(game.toMap())).then((r) => JSON.decode(r.response)).then(Game.fromMap);
+  Future<Game> delete(int id) => HttpRequest.request("api/games/$id.json", method: "DELETE").then((r) => JSON.decode(r.response)).then(Game.fromMap);
 }
 
 final GameStoreService gameStoreService = new GameStoreService();
